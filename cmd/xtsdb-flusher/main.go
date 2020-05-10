79f52cd8460d7b0c4ce18902d9318a1185de5c22
y_uuki <yuki.tsubo@gmail.com>
2020-05-10 09:00:02 +0900

    Add empty main package

---
+++ cmd/xtsdb-flusher/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"io"
+	"os"
+)
+
+// CLI is the command line object.
+type CLI struct {
+	// outStream and errStream are the stdout and stderr
+	// to write message from the CLI.
+	outStream, errStream io.Writer
+}
+
+func main() {
+	cli := &CLI{outStream: os.Stdout, errStream: os.Stderr}
+	os.Exit(cli.Run(os.Args))
+}
+
+// Run invokes the CLI with the given arguments.
+func (cli *CLI) Run(args []string) int {
+	return 0
+}
