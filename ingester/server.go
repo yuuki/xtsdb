0e4f2d039fec1a82b08cd513377c1e87e1c4f1b2
y_uuki <yuki.tsubo@gmail.com>
2020-06-05 01:18:28 +0900

    [ingester] Revert InsertCtx

--- ingester/server.go
+++ ingester/server.go
@@ -11,7 +11,6 @@ import (
 	"github.com/VictoriaMetrics/metrics"
 
 	"github.com/yuuki/xtsdb/storage"
-	"github.com/yuuki/xtsdb/storage/model"
 )
 
 var (
@@ -32,11 +31,10 @@ func insertHandler(r io.Reader) error {
 //
 // Copy code from github.com/VictoriaMetrics/VictoriaMetrics/app/vminsert/graphite.
 func insertRows(rows []vmparser.Row) error {
-	// ctx := GetInsertCtx()
-	// defer PutInsertCtx(ctx)
-	ctx := &InsertCtx{mrs: model.MetricRows{}}
+	ctx := GetInsertCtx()
+	defer PutInsertCtx(ctx)
 
-	// ctx.Reset(len(rows))
+	ctx.Reset(len(rows))
 	for i := range rows {
 		r := &rows[i]
 		ctx.Labels = ctx.Labels[:0]
